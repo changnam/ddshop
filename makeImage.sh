d91261139830662c24a64b3e062e5d23c7af8f59
root <root@k8smaster.netsec.kr>
2019-06-03 17:53:07 +0900

    v3.0

--- makeImage.sh
+++ makeImage.sh
@@ -1,3 +1,3 @@
 tar --exclude='./venv' -cvf ddshop.tar .
-docker build -t cngoh1/ddshop-app:2.0 -f Dockerfile-ddshop-app .
-docker build -t cngoh1/ddshop-web:2.0 -f Dockerfile-ddshop-web .
+docker build -t cngoh1/ddshop-app:3.0 -f Dockerfile-ddshop-app .
+docker build -t cngoh1/ddshop-web:3.0 -f Dockerfile-ddshop-web .
