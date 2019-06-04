f186a8d4d33e1d446c6fa014e327e20b1cb92d59
root <root@k8smaster.netsec.kr>
2019-06-04 19:25:02 +0900

    image tag lastest applied

--- makeImage.sh
+++ makeImage.sh
@@ -1,3 +1,3 @@
 tar --exclude='./venv' -cvf ddshop.tar .
-docker build -t cngoh1/ddshop-app:3.0 -f Dockerfile-ddshop-app .
-docker build -t cngoh1/ddshop-web:3.0 -f Dockerfile-ddshop-web .
+docker build -t cngoh1/ddshop-app -f Dockerfile-ddshop-app .
+docker build -t cngoh1/ddshop-web -f Dockerfile-ddshop-web .
