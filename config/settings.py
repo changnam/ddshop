20cd1201b20aafdd2205d3d830804d81b2a13a0a
test <aa@aa>
2019-06-16 15:18:38 +0900

    settings.py for static files

--- config/settings.py
+++ config/settings.py
@@ -159,4 +159,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
 
 CART_SESSION_ID = 'cart'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
